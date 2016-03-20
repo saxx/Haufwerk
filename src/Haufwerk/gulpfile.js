10733f0292dbc77deadb6e3e981bd564d73ec346
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2016-03-20 20:54:12 +0100

    Support for SASS and TypeScript.

--- src/Haufwerk/gulpfile.js
+++ src/Haufwerk/gulpfile.js
@@ -1,42 +1,33 @@
 ﻿var gulp = require("gulp"),
-    rimraf = require("rimraf"),
     concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
 
-var paths = {
-    webroot: "./wwwroot/"
-};
+    cssnano = require("gulp-cssnano"),
+    sass = require("gulp-sass"),
+    prefixer = require("gulp-autoprefixer"),
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+    uglify = require("gulp-uglify"),
+    ts = require("gulp-typescript");
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
-});
-
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
-});
-
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
+gulp.task("js", function () {
+    return gulp.src("./wwwroot/js/**/*.ts")
+        .pipe(ts({ module: "commonjs" }))
+        .pipe(concat("./wwwroot/js/site.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
+gulp.task("css", function () {
+    return gulp.src(["./wwwroot/css/**/*.scss"])
+        .pipe(sass().on("error", sass.logError))
+        .pipe(concat("./wwwroot/css/site.min.css"))
+        .pipe(prefixer({ browsers: ["last 3 versions"] }))
+        .pipe(cssnano())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task("watch", function () {
+    gulp.watch("./wwwroot/css/**/*.scss", ["css"]);
+    gulp.watch("./wwwroot/js/**/*.ts", ["js"]);
+});
+
+gulp.task("default", ["js", "css", "watch"]);
