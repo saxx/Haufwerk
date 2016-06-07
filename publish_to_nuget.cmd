21be3e5eb4fdeda9b657490aef5d43708f6f54a7
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2016-06-07 11:13:56 +0200

    Add missing -Source parameter to nuget publishing.

--- publish_to_nuget.cmd
+++ publish_to_nuget.cmd
@@ -1,4 +1,4 @@
 CALL dotnet pack ".\src\Haufwerk.Client" --configuration Release
-CALL nuget push ".\src\Haufwerk.Client\bin\Release\*.nupkg" %nuget_key%
+CALL nuget push ".\src\Haufwerk.Client\bin\Release\*.nupkg" %nuget_key% -Source https://www.nuget.org/api/v2/package
 
 exit /b 0
