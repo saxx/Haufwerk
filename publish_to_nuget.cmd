7a153097caf1c72b0c8421429b037646a7523b06
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2016-06-07 10:55:28 +0200

    Fix NuGet package generation.

--- publish_to_nuget.cmd
+++ publish_to_nuget.cmd
@@ -1,4 +1,4 @@
-CALL dnu pack ".\src\Haufwerk.Client" --configuration Release
+CALL dotnet pack ".\src\Haufwerk.Client" --configuration Release
 CALL nuget push ".\src\Haufwerk.Client\bin\Release\*.nupkg" %nuget_key%
 
 exit /b 0
